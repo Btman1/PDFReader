f422ab3fc363bd3c7f920c711e4ca7da0097c4ac
Btman1 <allentyao@gmail.com>
2017-07-01 13:40:41 -0700

    my first commitment

--- BJ projects/Scrapper/Scrapper/PDFReader.java
+++ BJ projects/Scrapper/Scrapper/PDFReader.java
@@ -1,80 +1,155 @@
-
-/**
- * Write a description of class APIthingy here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
-   
  
-
-
-import java.awt.image.BufferedImage;
+import java.io.IOException;
 import java.io.File;
-import javax.imageio.stream.ImageInputStream;
-import javax.imageio.stream.ImageOutputStream;
-import javax.imageio.ImageWriter;
-import java.util.Iterator;
-import javax.imageio.ImageIO;
-import com.sun.media.imageio.plugins.tiff.TIFFImageWriteParam;
-
-public class PDFReader
-{
-    // instance variables - replace the example below with your own
-    private int x;
+import java.io.*;
+import java.util.*;
+/**
+ * Contains some methods to list files and folders from a directory
+ *
+ * @author Loiane Groner
+ * http://loiane.com (Portuguese)
+ * http://loianegroner.com (English)
+ */
+public class PDFReader {
 
     /**
-     * Constructor for objects of class APIthingy
+     * List all the files and folders from a directory
+     * @param directoryName to be listed
      */
-    public PDFReader()
-    {
+    public void listFilesAndFolders(String directoryName){
+        File directory = new File(directoryName);
+        //get all the files from a directory
+        File[] fList = directory.listFiles();
+        for (File file : fList){
+            System.out.println(file.getName());
+        }
+    }
+    
+    /**
+     * List all the files under a directory
+     * @param directoryName to be listed
+     */
+    public ArrayList<String> listFiles(String directoryName){
+        ArrayList<String> ar = new ArrayList<String>();
+        File directory = new File(directoryName);
+        //get all the files from a directory
+        File[] fList = directory.listFiles();
+        for (File file : fList){
+            if (file.isFile()){
+                //System.out.println(file.getName());
+                ar.add(file.getName());
+            }
+        }
+        return ar;
+        
+    }
+    
+    private String[] filelistarray = new String[20];
+    
+    /**
+     * List all the folder under a directory
+     * @param directoryName to be listed
+     */
+    public void listFolders(String directoryName){
+        File directory = new File(directoryName);
+        //get all the files from a directory
+        File[] fList = directory.listFiles();
+        for (File file : fList){
+            if (file.isDirectory()){
+                System.out.println(file.getName());
+            }
+        }
+    }
+    /**
+     * List all files from a directory and its subdirectories
+     * @param directoryName to be listed
+     */
+    public void listFilesAndFilesSubDirectories(String directoryName){
+        File directory = new File(directoryName);
+        //get all the files from a directory
+        File[] fList = directory.listFiles();
+        for (File file : fList){
+            if (file.isFile()){
+                System.out.println(file.getAbsolutePath());
+            } else if (file.isDirectory()){
+                listFilesAndFilesSubDirectories(file.getAbsolutePath());
+            }
+        }
+    }
+    public static void runcmd (String tsr){
+        //tsr = ("/Users/ayao/Desktop/Folder/ponzi-scheme-politics-congress-obama-president-madoff-democr-demotivational-poster-1237071852.jpg");
+            try{
+            //String[] a = new String[] {"/bin/bash", "-c", "tesseract" + tsr + " /Users/ayao/Desktop/Folder/testingfirsttry.txt"};
+            String[] a = new String[] {"/bin/bash", "-c", tsr};
+            //String[] a = new String[] {"/bin/bash", "-c", "ls"};
+            ProcessBuilder pb = new ProcessBuilder(a);
+            Map<String, String> envs = pb.environment();
+            envs.put("PATH", "/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/opt/X11/bin");
+            
+            final Process proc = pb.start();
+            proc.waitFor();
+            InputStream is = proc.getInputStream();
+            InputStream stderr = proc.getErrorStream ();
+
+            BufferedReader errorreader = new BufferedReader (new InputStreamReader(stderr));
+
+            
+            
+            
+            InputStreamReader isr = new InputStreamReader(is);
+            BufferedReader br = new BufferedReader(isr);
+            String line;
+            while ((line = br.readLine()) != null) {
+              System.out.println(line);
+            }
+            while ((line = errorreader.readLine()) != null) {
+              System.out.println(line);
+            }
+            
+            System.out.println("Program terminated!");
+            //runcmd("/bin/bash -c 'touch /Users/ayao/Desktop/Folder/filetest.txt'");
+            //runcmd("ls -l");
+            
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+        catch(InterruptedException e){
+            e.printStackTrace();
+        } 
+     }
+     
+     
+    public static void main (String[] args){
+        PDFReader PDFReader = new PDFReader();
+        
+
+        
+        final String dirname ="/Users/ayao/Desktop/Folder/";
+        //Windows directory example
+        final String directoryWindows ="C://test";
+        ArrayList<String> lestring = PDFReader.listFiles(dirname);
+        for (int i = 0; i < lestring.size(); i++) {
+            String e = lestring.get(i);
+            String substr = e.substring(e.length() - 3);
+            //System.out.println("e " + e + "   substr:" + substr);
+            if (substr.equals("iff") || substr.equals("png")|| substr.equals("jpg") ) {
+                String tsr = ("tesseract '" + dirname + e + "' '" + dirname + e + ".txt'" );
+                System.out.println(tsr);
+                runcmd(tsr);
+            }
+            
+        }
+
+        String srcname = "ponzi-scheme-politics-congress-obama-president-madoff-democr-demotivational-poster-1237071852.jpg";
+
+        
+        
+
+        
+        
+    }
+    
   
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
-      	protected boolean saveTiff(String filename, BufferedImage image) {
-			
-		File tiffFile = new File(filename);
-		ImageOutputStream ios = null;
-		ImageWriter writer = null;
-		try {
-			
-			// find an appropriate writer
-			Iterator it = ImageIO.getImageWritersByFormatName("TIF");
-			if (it.hasNext()) {
-				writer = (ImageWriter)it.next();	
-			} else {
-				return false;
-			}
-			
-			// setup writer
-			ios = ImageIO.createImageOutputStream(tiffFile);
-			writer.setOutput(ios);
-			TIFFImageWriteParam writeParam = new TIFFImageWriteParam(Locale.ENGLISH);
-			writeParam.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
-			//  see writeParam.getCompressionTypes() for available compression type strings
-			writeParam.setCompressionType("PackBits"); 
-			
-			// convert to an IIOImage
-			IIOImage iioImage = new IIOImage(image, null, null);
-			// write it!
-			writer.write(null, iioImage, writeParam);
-			
-		} catch (IOException e) {
-			e.printStackTrace();
-			return false;
-		}
-		return true;		
-
-	}
+    
 }
